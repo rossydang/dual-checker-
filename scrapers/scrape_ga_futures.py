557ac751b416091f84ca28ea5bd4e946bf117ff8
Rossy Dang <rossy.d@igniterobotics.com>
2021-02-10 09:26:41 -0500

    added website and can now scrape ga futures

--- scrapers/scrape_ga_futures.py
+++ scrapers/scrape_ga_futures.py
@@ -1,5 +1,5 @@
-"""Scrapes the dual enrollment course directory"""
 
+import shelve
 import requests
 import bs4
 url = "https://www.gafutures.org/checs/DECourseDirectory/_DumpDataTable/?1607338557924&academic_yearinit=2021&partcollege_opeid=005601&categoryinit="
@@ -9,14 +9,28 @@ url = "https://www.gafutures.org/checs/DECourseDirectory/_DumpDataTable/?1607338
 #print (requests.get(url).text)
 
 #honestly I think using python's shelve module is fine enough 
+class Course:
+    
+    
+    def __init__(self, title, name, ID, academy):
+        self.title = title #IE ECON 1010
+        self.name = name #
+        self.ID = ID
+        self.academy = academy
+
+             
+        
+
+
 class College:
     """Each college has their own unique ID and data payload"""
     
     def __init__(self, name, ID, url):
-        self.name = name
+        self.college_name = name
         self.ID = ID
         self.url = url
-        
+       
+   
         
     def getCourses(self):
 
@@ -27,12 +41,51 @@ class College:
         #TODO debug this for all colleges
         table = courses.find("table")
         table_body = table.find ("tbody")
-        rows = table_body.find_all('tr')
+        rows = table_body.find_all('tr') #list of every table rows
         
-        print(rows)
         
-    def storeCourses():
-        pass                        
+        coursesList = []
+        #test_course = rows[0].contents
+        #print(test_course)
+        
+        for course in rows:
+            
+           # print (course.contents[1]) #CTAE place
+           # print  (course.contents[3]) #ID number
+           # print  (course.contents[5]) #title
+            print(course)
+            course_Store = Course(course.contents[5].get_text(), course.contents[3].get_text(), course.contents[1].get_text()) #TODO check if this works for every course in ga 
+            
+            coursesList.append(course_Store)
+            
+            self.storeCourses(coursesList)
+        
+        
+        
+    def storeCourses(self, courses):
+        #ID will be main key in storing courses
+        savedCourses = shelve.open(self.college_name)
+        
+        for course in courses:
+            if course.ID in savedCourses:
+                pass
+            
+            else:
+                #create entry 
+                
+        #BIG TODO figure out how to handle when the course directory deletes a course
+                
+            
+
+def initalize():
+    KSU = ""
+    GGC = ""
+    GaTech = ""
+    
+
+
+
+
         
 if __name__ == "__main__":
     c = College("DSD", "sadsa" ,url)
--- server.py
+++ server.py
@@ -1,25 +1,25 @@
 #Manages the server compnent. Use tkinter as a fallback
 
 
-@app.route('/', methods=["POST", "GET"])
-def index():
-    
-    if request.method == 'POST':
-        pass
-    return render_template("index.html")    
-    #flow of program: 
-    #index -> introduction to program
-    
+from flask import Flask, request, render_template
+app = Flask("dual")
 
     
 @app.route('/', methods=["POST", "GET"])
 def pickCollege():
+     if request.method == 'POST':
+        college_selected = request.form.get("college")
+        return college_selected
+     return render_template("college.html")
+
     #handles picking a college 
     #optional, add an AP score
-    pass
 
-@app.route('/', methods=["POST", "GET"])
+@app.route('/dasd', methods=["POST", "GET"])
 def showCouses():
     #show what courses are avalible at a college and its prerequistes
     pass
 
+if __name__ == '__main__':
+  
+    app.run(debug=True)
